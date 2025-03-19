94ae029380d525cf3b3228ed68c5522119983b24
Srivardhan00 <srivardhanveeramalli@gmail.com>
2025-03-19 22:00:06 +0530

    Enhance related files detection logic and add navigation button to clusters page

--- modules/utils.py
+++ modules/utils.py
@@ -9,6 +9,6 @@ def find_related_files(file_keywords):
     related_files = defaultdict(set)
     for file1, keywords1 in file_keywords.items():
         for file2, keywords2 in file_keywords.items():
-            if file1 != file2 and set(keywords1) & set(keywords2):
+            if file1 != file2 and len(set(keywords1) & set(keywords2))>1:
                 related_files[file1].add(file2)
     return related_files
